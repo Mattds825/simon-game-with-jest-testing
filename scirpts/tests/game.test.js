600fba62e1ce00f10db12ef5b1c604c89dc83128
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-08-23 10:38:44 +0100

    add tests to check keys in game class

---
+++ scirpts/tests/game.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const {game} = require('../game.js');
+
+beforeAll(() =>{
+    let fs = require('fs');
+    let fileContent = fs.readFileSync('index.html', 'utf8');
+
+    document.open();
+    document.write(fileContent);
+    document.close();
+});
+
+describe("Game object contains correct keys", ()=>{
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+});
